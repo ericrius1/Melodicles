3ff3ef665fcf560484fd65c5c98e0fb8fcca4203
Eric <Ericrius1@gmail.com>
2013-05-25 00:31:34 -0700

    added renderer

--- client/game/ml.Game.js
+++ client/game/ml.Game.js
@@ -10,6 +10,7 @@ ml.Game = new Class({
 
     this.SCREEN_WIDTH = window.innerWidth;
     this.SCREEN_HEIGHT = window.innerHeight;
+    this.MARGIN = 0;
   },
 
   init: function() {
@@ -26,6 +27,24 @@ ml.Game = new Class({
 
     //SCENE
     this.scene = new THREE.Scene();
+
+    //LIGHTS
+
+    this.light = new THREE.DirectionalLight (0xffffff);
+    this.light.position.set(0.5, 0.5, 1);
+    this.scene.add(this.light);
+
+    //RENDERER
+
+    this.renderer = new THREE.WebGLRenderer( {alpha: false});
+    this.renderer.setClearColor(0x050505);
+    this.renderer.setSize(this.SCREEN_WIDTH, this.SCREEN_HEIGHT);
+
+    this.renderer.domElement.style.position = "absolute";
+    this.renderer.domElement.style.top = this.MARGIN + "px";
+    this.renderer.domElement.style.left = "0px";
+
+    container.appendChild(this.renderer.domElement);
 
     this.comm = new ml.Comm({
       server: this.options.comm.server
