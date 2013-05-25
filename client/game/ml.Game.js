cc1d5b01cf36b4a3911a0cb7e0f160aa2db70fe5
Eric <Ericrius1@gmail.com>
2013-05-25 00:18:29 -0700

    more three.js stuff

--- client/game/ml.Game.js
+++ client/game/ml.Game.js
@@ -6,13 +6,26 @@ ml.Game = new Class({
   construct: function(options) {
     this.options = jQuery.extend({}, this.defaults, options);
     console.log("new game");
-    //this.init();
+    this.init();
+
+    this.SCREEN_WIDTH = window.innerWidth;
+    this.SCREEN_HEIGHT = window.innerHeight;
   },
 
   init: function() {
 
     container = document.createElement('div');
     document.body.appendChild(container);
+
+
+    //******THREE.JS SETUP******
+
+    //CAMERA
+    this.camera = new THREE.PerspectiveCamera(45, this.SCREEN_WIDTH / this.SCREEN_HEIGHT, 1, 10000);
+    this.camera.position.set(-500, 500, 1500);
+
+    //SCENE
+    this.scene = new THREE.Scene();
 
     this.comm = new ml.Comm({
       server: this.options.comm.server
