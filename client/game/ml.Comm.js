64b89c09e732427db0088de2fd67398ee7abc9db
Eric <Ericrius1@gmail.com>
2013-05-24 20:03:46 -0700

    adding socket events

---
+++ client/game/ml.Comm.js
@@ -0,0 +1,35 @@
+db.Comm = new Class({
+  extend: EventEmitter,
+  makeTrigger: function(evt) {
+    var that = this;
+    return function(message) {
+      that.trigger.call(that, evt, message);
+    }
+  },
+
+  destruct: function() {
+    //Todo: Seend server destruct message
+  },
+
+  construct: function(options) {
+    options = jQuery.extend({
+      server: 'localhost: 8080'
+    }, options);
+
+    options.server = 'http://' + options.server;
+
+    var that = this;
+
+    //Create socket connection
+    this.socket = io.connect(options.server);
+
+    this.socket.on('join', this.makeTrigger('join'));
+    this.socket.on('failed'),
+    function(message) {
+      //try to reconnect
+      that.connected();
+    }
+
+
+  }
+});
